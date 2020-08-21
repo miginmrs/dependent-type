8cac4fb684862be70d5e9e97541de7ef69f2a8ee
migin <miginmrs@protonmail.com>
2020-08-21 19:32:26 +0100

    fix async map

--- source/map.ts
+++ source/map.ts
@@ -6,3 +6,10 @@ export const depMap = <dom extends keytype, cim extends Record<Pair, [any, any]>
   arr.forEach((v, i) => r[i] = p(v, i));
   return r as cim[1][1][] & { [X in dom]: AppX<1, cim, k, X> };
 };
+
+
+export const asyncDepMap = async <dom extends keytype, cim extends Record<Pair, [any, any]>, k extends DepConstaint<Pair, dom, cim>>(arr: cim[0][1][], p: <X extends dom>(a: AppX<0, cim, k, X>, i: number) => AppX<1, cim, k, X> | PromiseLike<AppX<1, cim, k, X>>) => {
+  const r: cim[1][1][] = [];
+  await Promise.all(arr.map(async (v, i) => r[i] = await p(v, i)));
+  return r as cim[1][1][] & { [X in dom]: AppX<1, cim, k, X> };
+};
